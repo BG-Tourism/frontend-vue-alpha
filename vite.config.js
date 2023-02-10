46437497723ff2995a1aa52dac8096ef3c829310
XenBG <wildbeastbg@gmail.com>
2023-02-10 22:59:15 +0200

    Removed Prettier

--- vite.config.js
+++ vite.config.js
@@ -6,30 +6,30 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        host: 'localhost',
-        port: 3000
-    },
-    plugins: [
-        vue(),
-        topLevelAwait({
-            // The export name of top-level await promise for each chunk module
-            promiseExportName: '__tla',
-            // The function to generate import names of top-level await promise in each chunk module
-            promiseImportName: (i) => `__tla_${i}`
-        })
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  plugins: [
+    vue(),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`,
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
     ],
-    resolve: {
-        alias: [
-            {
-                find: '@',
-                replacement: path.resolve(__dirname, 'src')
-            }
-        ]
-    },
-    define: {
-        __VUE_I18N_FULL_INSTALL__: true,
-        __VUE_I18N_LEGACY_API__: false,
-        __INTLIFY_PROD_DEVTOOLS__: false
-    }
+  },
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
 })
